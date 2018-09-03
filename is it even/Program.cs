116ba190616312907355235908fafeba9ff43696
Kristen Shea <kstar61@live.com>
2018-09-03 18:08:36 -0500

    check of number is even or odd, print true for even and false for odd to console

--- is it even/Program.cs
+++ is it even/Program.cs
@@ -6,7 +6,27 @@ namespace is_it_even
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Is it an even number?");
+
+            //prompt user for a number, convert number to integer and assign a variable name
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //print true or false to console from method
+            Console.WriteLine(IsEven(number));
+            
+            Console.ReadLine();
         }
+        
+        /// <summary>
+        /// take user entered number and check to see if even or odd
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>true if number is even, false if it is odd</returns>
+        private static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }   
+
     }
 }
